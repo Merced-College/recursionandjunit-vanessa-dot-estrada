736d93ad32f55d132c9b0526aaf9992c0882e2e8
vanessa-dot-estrada <159970360+vanessa-dot-estrada@users.noreply.github.com>
2024-10-24 21:29:27 +0000

    Set All Tests

---
+++ Count8Test.java
@@ -0,0 +1,176 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Count8Test {
+    public void testCount1(){
+        // test cases
+        assertEquals(1, Recursion.count8(8));
+
+    }
+
+    @Test
+    public void testCount2(){
+        // test cases
+        assertEquals(2, Recursion.count8(88));
+        
+    }
+
+    @Test
+    public void testCount3(){
+        // test cases
+        assertEquals(1, Recursion.count8(1213128));
+        
+    }
+    
+    @Test
+    public void testCount4(){
+        // test cases
+        assertEquals(1, Recursion.count8(1213128));
+        
+    }
+    
+ // end of test for first method
+
+
+    @Test
+    public void testCount5(){
+        // test cases
+        assertEquals(1, Recursion.countHi("xxhixx"));
+        
+    }
+
+    @Test
+    public void testCount6(){
+        // test cases
+        assertEquals(2, Recursion.countHi("xxhixxhi"));
+        
+    }
+
+    @Test
+    public void testCount7(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount8(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount9(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    } // last test for CountHi
+
+
+    @Test
+    public void testCount10(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount11(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount12(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount13(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount14(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    } // last test for countHi2
+
+    @Test
+    public void testCount15(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount16(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount17(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount18(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount19(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    } // last test for cowCat prblem
+
+    @Test
+    public void testCount20(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount21(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount22(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount23(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    }
+
+    @Test
+    public void testCount24(){
+        // test cases
+        assertEquals(0, Recursion.countHi(null));
+        
+    } // last test for CleanString
+
+}
--- Recursion.java
+++ Recursion.java
@@ -3,7 +3,8 @@
 public class Recursion{
     public static void main(String[] args){
         // test cases
-        System.out.println(count8(n:8));   // expected output: 1
+        System.out.println(count8(8));   // expected output: 1
+    }
 
         public static int count8(int n) {
             // base case if N is 0, ther eare no digits to count
@@ -12,9 +13,6 @@ public class Recursion{
             }
             // check the rightmost digit
             int rightmostDigit = n % 10;
-
-            // check the next digit to the left
-            int nextDigit = (n / 10) % 10;
 
             // if the right most digit is 8
             if (rightmostDigit == 8) {
@@ -26,5 +24,55 @@ public class Recursion{
             } // End else
         
     } // end of method
+
+    // next Problem
+
+    public static int countHi(String str){
+        // check for null
+        if (str == null)
+            return 0;
+
+
+        // base case
+        if (str.length() < 2)
+            return 0;
+
+
+        char char1 = str.charAt(0);
+        char char2 = str.charAt( 1);
+
+        //check if char1 and h  char2 is an i
+        if(char1 == 'h' && char2 == 'i')
+            return 1 + countHi(str.substring(2)); // send tin the string minus the first 2 char
+
+        return countHi(str.substring(1)); // send in the string minus the first char
+
+    }
+
+    // next Problem
+
+    public static int countHi2(String str){
+        // check for null
+        if (str == null)
+            return 0;
+
+
+        // base case
+        if (str.length() < 2)
+            return 0;
+
+
+        char char1 = str.charAt(0);
+        char char2 = str.charAt( 1);
+
+        //check if char1 and h  char2 is an i
+        if(char1 == 'h' && char2 == 'i')
+            return 1 + countHi(str.substring(2)); // send tin the string minus the first 2 char
+
+        return countHi(str.substring(1)); // send in the string minus the first char
+
+    }
+
+
 
 } // end of class
